ea43379f9a20fb206308e0481336eb24c7151f9f
shahla <shahlafebinta@gmail.com>
2023-10-08 12:37:42 +0530

    commit

---
+++ my-project/src/component/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../Header/Header.css'
+
+
+function header() {
+  return (
+
+    <div className='Headb'>
+      <h6 className='Head'><span></span>WELCOME</h6>
+
+    
+    </div>
+  
+  )
+}
+
+export default header
